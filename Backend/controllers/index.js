aaf3b53e04f7255b83f1c95caa5ffa4112f74999
Rishabh Porwal <rishabhporwal95@gmail.com>
2023-06-12 18:06:46 +0530

    feature: created API for getting category by category id

--- Backend/controllers/index.js
+++ Backend/controllers/index.js
@@ -4,7 +4,7 @@ const Category = require("../models/category");
 exports.getCategories = async (req, res) => {
   try {
     const categories = await Category.findAll({ include: Category });
-    return res.status(200).json({ categories });
+    res.status(200).json({ categories });
   } catch (error) {
     console.error(err);
     res.status(500).json({ error: "An error occurred" });
@@ -16,8 +16,24 @@ exports.createCategories = async (req, res) => {
   try {
     const { name, parentId } = req.body;
     const category = await Category.create({ name, parentId });
-    return res.status(200).json({ category });
+    res.status(200).json({ category });
   } catch (error) {
-    return res.status(500).json({ error: "An error occurred" });
+    res.status(500).json({ error: "An error occurred" });
+  }
+};
+
+// Retrieve a single category by ID
+exports.getCategoriesById = async (req, res) => {
+  try {
+    const categoryId = req.params.id;
+    const category = await Category.findByPk(categoryId, { include: Category });
+
+    if (!category) {
+      res.status(404).json({ error: "Category not found" });
+    }
+    res.status(200).json({ category });
+  } catch (error) {
+    console.error(err);
+    res.status(500).json({ error: "An error occurred" });
   }
 };
--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -2,9 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
-const { getCategories, createCategories } = require("../controllers/index");
+const {
+  getCategories,
+  getCategoriesById,
+  createCategories,
+} = require("../controllers/index");
 
 router.get("/categories", getCategories);
+router.get("/categories/:id", getCategoriesById);
 router.post("/categories", createCategories);
 
 module.exports = router;
