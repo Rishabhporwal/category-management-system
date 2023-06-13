ebc81ff0eab2567a299c2aed0d18b4f2bae77b45
Rishabh Porwal <rishabhporwal95@gmail.com>
2023-06-14 04:58:14 +0530

    feature: added logs and base condition in update and delete function

--- Backend/controllers/index.js
+++ Backend/controllers/index.js
@@ -18,7 +18,7 @@ exports.getCategories = async (req, res) => {
     // Log error
     console.error(error);
     // Send failed 500 response
-    return res.status(500).json({ error: "An error occurred: ${error}" });
+    return res.status(500).json({ error: `An error occurred: ${error}` });
   }
 };
 
@@ -96,55 +96,137 @@ exports.createCategories = async (req, res) => {
   }
 };
 
-// Retrieve a single category by ID
-exports.getCategoriesById = async (req, res) => {
-  try {
-    const categoryId = req.params.id;
-    const category = await Category.findByPk(categoryId, { include: Category });
-
-    if (!category) {
-      return res.status(404).json({ error: "Category not found" });
-    }
-    return res.status(200).json({ category });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "An error occurred" });
-  }
-};
-
 // Update a category by ID
 exports.updateCategory = async (req, res) => {
   try {
     const categoryId = req.params.id;
     const { name, parentId } = req.body;
+
+    // checking for name in request
+    if (!name) {
+      // throwing 400 error
+      return res.status(400).send({
+        message: "Name is missing",
+      });
+    }
+
+    // checking for parent id in request
+    if (!parentId) {
+      // throwing 400 error
+      return res.status(400).send({
+        message: "Parentid is missing",
+      });
+    }
+
+    // getting category with categoryid
     const category = await Category.findByPk(categoryId);
     if (!category) {
       return res.status(404).json({ error: "Category not found" });
     }
 
+    if (category.parentId) {
+      // Fetch Parent Category to be update
+      const parent = await Category.findByPk(category.parentId);
+
+      // Checking if parent category exists
+      if (!parent) {
+        // Throw 400 Error
+        return res.status(400).send({
+          message: "Parent category not avalable",
+        });
+      }
+
+      // Check if name we are getting and name from db is same
+      if (parent.name === name) {
+        // throwing 400 error
+        return res.status(400).send({
+          message: "Category is already available",
+        });
+      }
+
+      // Fetching categories with same parent category id
+      const children = await Category.findAll({
+        where: { parentId },
+      });
+
+      // Checking for same children names
+      const isChildPresent = children.some((child) => child.name === name);
+      if (isChildPresent) {
+        // throwing 400 error
+        return res.status(400).send({
+          message: "Category is already available",
+        });
+      }
+    } else {
+      // fetching all root categories
+      const root = await Category.findAll({
+        where: { parentId: null },
+      });
+
+      // Checking for same names
+      const isParentPresent = root.some((item) => item.name === name);
+      if (isParentPresent) {
+        // throwing 400 error
+        return res.status(400).send({
+          message: "Category is already available",
+        });
+      }
+    }
+
     category.name = name;
     category.parentId = parentId;
+
+    // Updating Categories
     await category.save();
+    //sending 200 error response
     return res.status(200).json({ category });
   } catch (error) {
+    // Log error
     console.error(error);
-    return res.status(500).json({ error: "An error occurred" });
+    // Send failed 500 response
+    return res.status(500).json({ error: `An error occurred: ${error}` });
   }
 };
 
 // Delete a category by ID
 exports.deleteCategory = async (req, res) => {
   try {
-    const categoryId = req.params.id;
-    const category = await Category.findByPk(categoryId);
-    if (!category) {
-      return res.status(404).json({ error: "Category not found" });
+    const parentId = req.params.id;
+
+    // Checking category id is not empty
+    if (!id) {
+      // throwing 400 error
+      res.status(400).send({
+        message: "Category id is missing",
+      });
     }
 
-    await category.destroy();
-    return res.status(204).json(`Catogory ${category.name} deleted`);
+    // Fetching categories with same parent category id
+    const children = await Category.findAll({
+      where: { parentId },
+    });
+
+    if (children.length > 0) {
+      // throwing 400 error
+      res.status(400).send({
+        message: "Please delete child categories first",
+      });
+    }
+
+    // Deleting the category
+    const result = await category.destroy();
+    if (result !== 1) {
+      // throwing 400 error
+      res.status(400).send({
+        message: "Cannot delete category",
+      });
+    }
+    //sending 200 error response
+    return res.status(204).json(`Catogory id: ${parentId} deleted`);
   } catch (error) {
+    // Log error
     console.error(error);
-    return res.status(500).json({ error: "An error occurred" });
+    // Send failed 500 response
+    return res.status(500).json({ error: `An error occurred: ${error}` });
   }
 };
--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -4,14 +4,12 @@ const router = express.Router();
 
 const {
   getCategories,
-  getCategoriesById,
   createCategories,
   updateCategory,
   deleteCategory,
 } = require("../controllers/index");
 
 router.get("/categories", getCategories);
-router.get("/categories/:id", getCategoriesById);
 router.post("/categories", createCategories);
 router.put("/categories/:id", updateCategory);
 router.delete("/categories/:id", deleteCategory);
